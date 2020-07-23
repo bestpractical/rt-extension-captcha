ad02a6ce25ec77116e04471ae2d4dd39289fd2fc
sunnavy <sunnavy@bestpractical.com>
2020-07-23 20:59:23 +0800

    Default RT path to /opt/rt5

--- lib/RT/Extension/Captcha.pm
+++ lib/RT/Extension/Captcha.pm
@@ -30,7 +30,7 @@ create) and on replies/comments (updates).
 
 May need root permissions
 
-=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
+=item Edit your F</opt/rt5/etc/RT_SiteConfig.pm>
 
 Add this line:
 
@@ -38,7 +38,7 @@ Add this line:
 
 =item Clear your mason cache
 
-    rm -rf /opt/rt4/var/mason_data/obj
+    rm -rf /opt/rt5/var/mason_data/obj
 
 =item Restart your webserver
 
